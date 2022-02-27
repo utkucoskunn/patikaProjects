edc8e7a0e4ad809edc6dce02a620c671a02277bb
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-02-27 08:37:56 +0300

    Update project_5_11_Exponent_Calculator_Program.java

--- project_5_11_Exponent_Calculator_Program.java
+++ project_5_11_Exponent_Calculator_Program.java
@@ -3,12 +3,20 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) throws Exception {
 
+
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter number one :  ");
         int n = input.nextInt();
         System.out.print("Please enter number two :  ");
         int r = input.nextInt();
+        int result=1;
+       /* System.out.println(n+" pow "+r +": "+ Math.pow(n,r));*/
 
-        System.out.println(n+" pow "+r +": "+ Math.pow(n,r));
+        for (int i=r;i>=1;i--){
+
+           result*=n;
+        }
+
+        System.out.println(n+" pow "+r +": "+ result);
     }
 }
